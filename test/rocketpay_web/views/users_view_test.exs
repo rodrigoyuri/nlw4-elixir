cb2c71a2d5422b221980ae46f73e3de64f261e3c
Rodrigo Yuri <rodrigo33996@gmail.com>
2021-02-28 17:00:51 -0300

    test de view

---
+++ test/rocketpay_web/views/users_view_test.exs
@@ -0,0 +1,38 @@
+defmodule RocketpayWeb.UsersViewTest do
+  use RocketpayWeb.ConnCase, async: true
+
+  import Phoenix.View
+
+  alias Rocketpay.{User, Account}
+  alias RocketpayWeb.UsersView
+
+  test "renders create.json" do
+    params = %{
+      name: "Rodrigo",
+      password: "123456",
+      nickname: "rodrigo",
+      email: "rodrigo@gmail.com",
+      age: 21,
+    }
+
+    {:ok, %User{id: user_id, account: %Account{id: account_id}} = user} =
+      Rocketpay.create_user(params)
+
+    response = render(UsersView, "create.json", user: user)
+
+    expected_response = %{
+      message: "User created",
+      user: %{
+        account: %{
+          balance: Decimal.new("0.00"),
+          id: account_id
+        },
+        id: user_id,
+        name: "Rodrigo",
+        nickname: "rodrigo"
+      }
+    }
+
+    assert expected_response == response
+  end
+end
