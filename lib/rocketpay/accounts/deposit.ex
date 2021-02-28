d49e4dea9507159f4a4105af631cb7404fc89b31
Rodrigo Yuri <rodrigo33996@gmail.com>
2021-02-28 12:05:03 -0300

    mudança na ordem na função add da lib Decimal

--- lib/rocketpay/accounts/deposit.ex
+++ lib/rocketpay/accounts/deposit.ex
@@ -31,7 +31,7 @@ defmodule Rocketpay.Accounts.Deposit do
     |> handle_cast(balance)
   end
 
-  defp handle_cast({:ok, value}, balance), do: Decimal.add(value, balance)
+  defp handle_cast({:ok, value}, balance), do: Decimal.add(balance, value)
   defp handle_cast(:error, _balance), do: {:error, "Invalid deposti value!"}
 
   defp update_account({:error, _reason} = error, _repo, _account), do: error
